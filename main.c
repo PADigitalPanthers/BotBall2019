0c4acf224fef63189cdf264c3fc17d599106f14f
PADigitalPanthers <48360590+PADigitalPanthers@users.noreply.github.com>
2019-04-17 14:07:19 -0700

    Final Code for Grandma

--- main.c
+++ main.c
@@ -1,73 +1,105 @@
 #include <kipr/botball.h>
 
 int main()
-    
-{ 
+{   
     wait_for_light(0);
-    
-    enable_servos();
-    set_servo_position(0,822);
-    set_servo_position(3,1111);
-    mav(0,1000);	//Go forward
-    mav(3,1000);
-    msleep(3300);
-    mav(0,500);		// turn RIGHT
-    mav(3,-500);
-    msleep(2850);
-    mav(0,1000);	// collect poms
-    mav(3,1000);
-    set_servo_position(0,2047);
-    set_servo_position(3,0);
-    msleep(9000);
-    mav(0,500);		// turn RIGHT
-    mav(3,-500);
-    msleep(2850);
-    mav(0,1000);	//go twords zone
-    mav(3,1000);
-    msleep(3000);
+    wait_for_milliseconds(20000);
+    enable_servos(1);
+    set_servo_position(1,0);
+    motor(3,-90);
+    motor(1,90);
+    wait_for_milliseconds(2000);
     ao();
-    msleep(2000);
-    mav(0,-1000);	//go back
-    mav(3,-1000);
-    msleep(2500);
-    mav(0,500);		// uturn
-    mav(3,-500);
-    msleep(5800);
-    mav(0,1000);	
-    mav(3,1000);
-    msleep(300);
-    set_servo_position(0,983);	//ajust claw
-    set_servo_position(3,718);
-    msleep(1700);
-    mav(0,1000);
-    mav(3,1000);
-    msleep(300);
-    set_servo_position(0,778);	//grab block
-    set_servo_position(3,1144);
-    mav(0,-1000);
-    mav(3,-1000);
-    msleep(2000);
-    mav(0,500);		// uturn
-    mav(3,-500);
-    msleep(5600);
-    mav(0,1000);
-    mav(3,1000);
-    msleep(1900);
-    ao();
+    enable_servos(0);
     set_servo_position(0,2047);
-    set_servo_position(3,0);
-    msleep(1000)
-    mav(0,-1000);	//go back
-    mav(3,-1000);
-    msleep(2500);
-    mav(0,-500);		// turn RIGHT
-    mav(3,500);
-    msleep(2850);
-    mav(0,1000);
-    mav(3,1000);
-    msleep(7000);
-    set_servo_position(0,822);
-    set_servo_position(3,1111);
-    
+    wait_for_milliseconds(1000);
+    motor(3,-60);
+    motor(1,60);
+    wait_for_milliseconds(1000);
+    ao();
+    motor(3, -100);
+    motor(1, 100);
+    wait_for_milliseconds(1000);
+    ao();
+    motor(3, -100);
+    motor(1, 100);
+    wait_for_milliseconds(1000);
+    ao();
+    motor(3, -100);
+    motor(1, 100);
+    wait_for_milliseconds(1000);
+    ao();
+    motor(3,-100);
+    motor(1,100);
+    wait_for_milliseconds(1000);
+    ao();
+    motor(3,-100);
+    motor(1,100);
+    wait_for_milliseconds(1000);
+    ao();
+    motor(3,-40);
+    motor(1,40);
+    wait_for_milliseconds(1000);
+    ao();
+    motor(3,-55);
+    motor(1,55);
+    wait_for_milliseconds(1000);
+    ao();
+    motor(3,-100);
+    motor(1,100);
+    wait_for_milliseconds(1000);
+    ao();
+    motor(3,-100);
+    motor(1,100);
+    wait_for_milliseconds(1000);
+    ao();
+    motor(3,-50);
+    motor(1,50);
+    wait_for_milliseconds(1000);
+    ao();
+    enable_servos(1);
+    set_servo_position(1,881);
+    wait_for_milliseconds(1000);
+    ao();
+    motor(3,-100);
+    motor(1,100);
+    wait_for_milliseconds(1000);
+    ao();
+    motor(3,-100);
+    motor(1,100);
+    wait_for_milliseconds(1000);
+    ao();
+    motor(3,-100);
+    motor(1,100);
+    wait_for_milliseconds(1000);
+    ao();
+    motor(3,-100);
+    motor(1,100);
+    wait_for_milliseconds(1000);
+    ao();
+    motor(3,-100);
+    motor(1,100);
+    wait_for_milliseconds(1000);
+    ao();
+    motor(3,-100);
+    motor(1,100);
+    wait_for_milliseconds(1000);
+    ao();
+    motor(3,-100);
+    motor(1,100);
+    wait_for_milliseconds(1000);
+    ao();
+    motor(3,-100);
+    motor(1,100);
+    wait_for_milliseconds(1000);
+    ao();
+    enable_servos(0);
+    set_servo_position(0,0);
+    wait_for_milliseconds(1000);
+    ao();
+    enable_servos(1);
+    set_servo_position(1,0);
+    wait_for_milliseconds(1000);
+    ao();
     return 0;
 }
